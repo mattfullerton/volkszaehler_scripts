ce305e50b317add246670b90c998dc1555c8eda1
Matt Fullerton <matt.fullerton@gmail.com>
2022-11-20 14:39:55 +0100

    Create run.sh

---
+++ readHeat/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+cd /root/scripts/mbus
+rm startup.txt
+rm output.txt
+echo "1" | ./main > startup.txt &
+sleep 3
+pkill -9 main &
+sleep .5
+echo "3" | ./main > output.txt &
+sleep 40
+pkill -9 main &
+sleep 20
+cat output.txt
+python3 readHeat.py
+shutdown +5
