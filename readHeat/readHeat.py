b43044e5ad6d89042ca433884001a1be3fde9e90
Matt Fullerton <matt.fullerton@gmail.com>
2023-03-05 17:55:40 +0100

    Disable momentary power

--- readHeat/readHeat.py
+++ readHeat/readHeat.py
@@ -2,10 +2,12 @@
 import requests
 
 meter_channel = "77bde810-65ef-11ed-a456-d344cac2e6de"
-power_channel = "efc98650-6a33-11ed-b409-318a2275d4f2"
+# Uncomment and add UUID for momentary power
+#power_channel = ""
 
 mvalue = None
-pvalue = None
+# Uncomment for momentary power
+#pvalue = None
 
 with open("output.txt", "r") as ofile:
     while True:
@@ -20,19 +22,26 @@ with open("output.txt", "r") as ofile:
             mvalue = int((valueLine.split("result: ")[1].split("\t")[0]))
             if mvalue > 0:
                 print("Read " + str(mvalue) + " kWh")
-        if "Power (W)" in line:
-            ofile.readline()
-            valueLine = ofile.readline()
-            pvalue = int((valueLine.split("result: ")[1].split("\t")[0]))
-            print("Read " + str(pvalue) + " W")
+            # Remove the following line and uncomment the next block
+            # if you want to collect the momentary power.
+            # As it is only a snapshot if you are running only
+            # 4 times a day, it isn't very useful.
             break
+#        if "Power (W)" in line:
+#            ofile.readline()
+#            valueLine = ofile.readline()
+#            pvalue = int((valueLine.split("result: ")[1].split("\t")[0]))
+#            print("Read " + str(pvalue) + " W")
+#            break
 
 if mvalue is not None:
     print("Sending to Volkszähler")
     pushUrl = "http://192.168.0.215/middleware/data/" + meter_channel + ".json?operation=add&value=" + str(mvalue)
     print("Sending request: " + pushUrl)
     pushResult = requests.get(pushUrl)
-if pvalue is not None:
-    pushUrl = "http://192.168.0.215/middleware/data/" + power_channel + ".json?operation=add&value=" + str(pvalue)
-    print("Sending request: " + pushUrl)
-    pushResult = requests.get(pushUrl)
+
+# Uncomment for momentary power
+#if pvalue is not None:
+#    pushUrl = "http://192.168.0.215/middleware/data/" + power_channel + ".json?operation=add&value=" + str(pvalue)
+#    print("Sending request: " + pushUrl)
+#    pushResult = requests.get(pushUrl)
